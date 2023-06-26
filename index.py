8ac04c45398a395948d674390fba4b768569988a
Tiago Kepe <tiagokepe@gmail.com>
2023-06-26 19:10:27 -0300

    Added handle_event method,

--- index.py
+++ index.py
@@ -22,3 +22,27 @@ def get_balance():
     except KeyError:
         return "0", 404
 
+@app.route('/event', methods=['POST'])
+def handle_event():
+    event = request.get_json()
+    if event['type'] == 'deposit':
+        account = ac_controller.deposit(Account(id=event['destination'], balance=event['amount']))
+        return json.dumps({"destination": account.dict()}), 201
+
+    if event['type'] == 'withdraw':
+        try:
+            account = ac_controller.withdraw(Account(id=event['origin'], balance=event['amount']))
+            return json.dumps({"origin": account.dict()}), 201
+        except KeyError:
+            return "0", 404
+
+    if event['type'] == 'transfer':
+        try:
+            orig_ac = Account(id=event['origin'], balance=event['amount'])
+            dest_ac = Account(id=event['destination'])
+            orig_ac, dest_ac = ac_controller.transfer(orig_ac, dest_ac)
+            return json.dumps({"origin": orig_ac.dict(), "destination": dest_ac.dict()}), 201
+        except KeyError:
+            return "0", 404
+
+    return "Unrecognized event", 501
